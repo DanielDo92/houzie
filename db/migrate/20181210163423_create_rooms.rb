cdbcc6dda6e6334c36156ac7d026264bb6e0074e
DaiDuongPixta <duong.dodai@pixta.co.jp>
2018-12-22 00:21:08 +0700

    Create devise user

--- db/migrate/20181210163423_create_rooms.rb
+++ db/migrate/20181210163423_create_rooms.rb
@@ -1,9 +1,15 @@
 class CreateRooms < ActiveRecord::Migration
   def change
     create_table :rooms do |t|
-      t.string :name
+      t.string :name, null: false
+      t.integer :level, null: false
+      t.integer :price, null: false, default: 1_000_000
+      t.integer :occupants, null: false, default: 0
+      t.decimal :area, null: false, precision: 10, scale: 2
+      t.boolean :suited, null: false, default: true
 
       t.timestamps null: false
     end
   end
 end
+
